7b20029ac3e673ebb865c95aa14ab7a9aa5ab072
BanYeah <04smailing@gmail.com>
2026-07-11 11:46:20 +0900

    Fix: arduino drive

--- drive_keyboard.py
+++ drive_keyboard.py
@@ -16,40 +16,38 @@ ser = serial.Serial(PORT, BAUD, timeout=0)
 time.sleep(2)
 
 print("Keyboard control start")
-print("w: forward")
-print("x: backward")
-print("a: steering A")
-print("d: steering D")
+print("w:     forward")
+print("a:     steering left")
+print("s:     backward")
+print("d:     steering right")
 print("space: stop")
-print("esc: quit")
+print("esc:   quit")
 
 try:
     while True:
-        if keyboard.is_pressed("esc"):
-            ser.write(b"s")
-            print("Quit")
-            break
-
         if keyboard.is_pressed("w"):
-            ser.write(b"w")
-
-        if keyboard.is_pressed("x"):
-            ser.write(b"x")
+            ser.write(b"w\n")
 
         if keyboard.is_pressed("a"):
-            ser.write(b"a")
+            ser.write(b"a\n")
+
+        if keyboard.is_pressed("s"):
+            ser.write(b"s\n")
 
         if keyboard.is_pressed("d"):
-            ser.write(b"d")
+            ser.write(b"d\n")
 
         if keyboard.is_pressed("space"):
-            ser.write(b"s")
+            ser.write(b"stop\n")
+
+        if keyboard.is_pressed("esc"):
+            ser.write(b"stop\n")
+            break
 
         time.sleep(SEND_INTERVAL)
 
 except KeyboardInterrupt:
-    ser.write(b"s")
-    print("Stopped by Ctrl+C")
+    ser.write(b"stop\n")
 
 finally:
     ser.close()
